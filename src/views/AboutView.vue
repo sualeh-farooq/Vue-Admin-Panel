eaba8dda1a86fb8907968e55708b541ae008cb82
Yaqoob <sualehfarooq65@gmail.com>
2023-01-17 19:19:25 +0500

    commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Menu></Menu>
   </div>
 </template>
+
+
+<script>
+import Menu from '@/components/Menu.vue';
+export default {
+components : {
+  Menu
+}
+}
+</script>
